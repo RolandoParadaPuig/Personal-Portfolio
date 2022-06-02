37a1eab1cd071ca5f742bb30903f4662648b12c3
Rolando Parada <rolandoparadapuig@gmail.com>
2022-06-02 15:41:11 -0400

    title changed

--- src/components/layout/LayoutComponent.jsx
+++ src/components/layout/LayoutComponent.jsx
@@ -1,14 +1,18 @@
+import { Layout } from "antd";
 import React from "react";
+import { Outlet } from "react-router";
 import { ContentComponent } from "../contentComponent/ContentComponent";
 import { HeaderComponent } from "../headerComponent/HeaderComponent";
 import { NavComponent } from "../navComponent/NavComponent";
-
+const { Content } = Layout;
 export const LayoutComponent = () => {
   return (
     <>
       <HeaderComponent />
       <NavComponent />
-      <ContentComponent />
+      <Content>
+        <Outlet />
+      </Content>
     </>
   );
 };
