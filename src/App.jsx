d6afccb49b7cbcfebdc3be1f4b1127066a10b8ed
Rolando Parada <rolandoparadapuig@gmail.com>
2022-06-14 23:52:22 -0400

    added time-tracking component

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { LayoutComponent } from "./components/layout/LayoutComponent";
 import { ChartContainer } from "./components/expencesChartComponent/components/chartContainer/ChartContainer";
 import { IntroSectionContainer } from "./components/introSectionContainer/IntroSectionContainer";
 import { AdviceComponent } from "./components/adviceGeneratorComponent/components/adviceGeneratorComponent/AdviceComponent";
+import { TimeTrackingComponent } from "./components/timeTrackingComponent/components/timeTrackingComponent/TimeTrackingComponent";
 function App() {
   const [count, setCount] = useState(0);
 
@@ -20,6 +21,7 @@ function App() {
           <Route path={"/expences-chart"} element={<ChartContainer />} />
           <Route path={"/intro-section"} element={<IntroSectionContainer />} />
           <Route path={"/advice-generator"} element={<AdviceComponent />} />
+          <Route path={"/time-tracking"} element={<TimeTrackingComponent />} />
         </Routes>
       </Router>
     </>
