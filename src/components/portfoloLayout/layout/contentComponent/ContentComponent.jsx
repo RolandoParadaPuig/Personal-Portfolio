a7e189f770a9548218781e37faa0f9d82ba63ca8
Rolando Parada <rolandoparadapuig@gmail.com>
2022-06-18 15:55:32 -0400

    time traking fixed

--- src/components/portfoloLayout/layout/contentComponent/ContentComponent.jsx
+++ src/components/portfoloLayout/layout/contentComponent/ContentComponent.jsx
@@ -3,18 +3,17 @@ import "./contentComponent.css";
 
 import { About } from "./about/About";
 import { Tecnologies } from "./tecnologies/Tecnologies";
+import { MyWork } from "./myWork/MyWork";
 export const ContentComponent = () => {
   return (
     <main className="portfolio--main-content">
       <About />
       <Tecnologies />
-      <section id="work" className={"work"}>
-        hi x 3
-      </section>
-      <section id="contact" className={"contact"}>
+      <MyWork />
+      <section id="contact" className={"portfolio--contact"}>
         hi x 4
       </section>
-      <section id="social" className={"social"}>
+      <section id="social" className={"portfolio--social"}>
         hi x 4
       </section>
     </main>
---
+++ src/components/portfoloLayout/layout/contentComponent/myWork/MyWork.jsx
@@ -0,0 +1,90 @@
+import React from "react";
+import "./myWork.css";
+export const MyWork = () => {
+  return (
+    <section id="work" className={"portfolio--work"}>
+      <div className="work-grid-container">
+        <div className="work-page-container">
+          <img alt="Page-work" className="work-page-image" />
+
+          <a href="#">
+            <h2>pageTitle</h2>
+          </a>
+          <div className="work-page-tags">tags</div>
+          <p>description</p>
+          <a href="#">GitHub</a>
+        </div>
+        <div className="work-page-container">
+          <img alt="Page-work" className="work-page-image" />
+
+          <a href="#">
+            <h2>pageTitle</h2>
+          </a>
+          <div className="work-page-tags">tags</div>
+          <p>description</p>
+          <a href="#">GitHub</a>
+        </div>
+        <div className="work-page-container">
+          <img alt="Page-work" className="work-page-image" />
+
+          <a href="#">
+            <h2>pageTitle</h2>
+          </a>
+          <div className="work-page-tags">tags</div>
+          <p>description</p>
+          <a href="#">GitHub</a>
+        </div>
+        <div className="work-page-container">
+          <img alt="Page-work" className="work-page-image" />
+
+          <a href="#">
+            <h2>pageTitle</h2>
+          </a>
+          <div className="work-page-tags">tags</div>
+          <p>description</p>
+          <a href="#">GitHub</a>
+        </div>
+        <div className="work-page-container">
+          <img alt="Page-work" className="work-page-image" />
+
+          <a href="#">
+            <h2>pageTitle</h2>
+          </a>
+          <div className="work-page-tags">tags</div>
+          <p>description</p>
+          <a href="#">GitHub</a>
+        </div>
+        <div className="work-page-container">
+          <img alt="Page-work" className="work-page-image" />
+
+          <a href="#">
+            <h2>pageTitle</h2>
+          </a>
+          <div className="work-page-tags">tags</div>
+          <p>description</p>
+          <a href="#">GitHub</a>
+        </div>
+        <div className="work-page-container">
+          <img alt="Page-work" className="work-page-image" />
+
+          <a href="#">
+            <h2>pageTitle</h2>
+          </a>
+          <div className="work-page-tags">tags</div>
+          <p>description</p>
+          <a href="#">GitHub</a>
+        </div>
+        <div className="work-page-container">
+          <img alt="Page-work" className="work-page-image" />
+
+          <a href="#">
+            <h2>pageTitle</h2>
+          </a>
+          <div className="work-page-tags">tags</div>
+          <p>description</p>
+          <a href="#">GitHub</a>
+        </div>
+      </div>
+    </section>
+  );
+};
